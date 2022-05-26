09149d4dd303d38e7ab07b09f3f1fc9d4d5cb096
EidHachem <eidhachem1@gmail.com>
2022-05-26 20:13:38 +0300

    Add expenses section using props

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css';
+import ExpensesDate from './ExpensesDate';
+import Card from './Card';
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpensesDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/Components/ExpensesDate.js
@@ -0,0 +1,17 @@
+import './ExpensesDate.css';
+
+function ExpensesDate(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{day}</div>
+      <div className="expense-date__day">{year}</div>
+    </div>
+  );
+}
+
+export default ExpensesDate;
